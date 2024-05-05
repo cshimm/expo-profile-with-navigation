f0c29bd581ee72c929e32d129e5ae4b0731cd055
cshimm <cameronjshimmin@gmail.com>
2024-05-05 11:38:00 -0700

    image picker added to home page

--- app/Home.js
+++ app/Home.js
@@ -1,11 +1,29 @@
-import {StyleSheet, Text} from 'react-native'
+import {Image, Pressable, StyleSheet, Text} from 'react-native'
 import LinkToPage from "../components/LinkToPage";
 import {projects} from "../data/projects";
+import {requestMediaLibraryPermissionsAsync, launchImageLibraryAsync} from "expo-image-picker";
+import {useState} from "react";
 
 export default function Home() {
+    const [img, setImg] = useState('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9MJ4B-SWqlGeomYy4rO0GMOJO3g9nYJKfCPzgkaAAYw&s')
+    let openImagePickerAsync = async () => {
+        let permissionResult = await requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
+        }
+
+        let pickerResult = await launchImageLibraryAsync();
+        setImg(pickerResult.assets[0].uri)
+    }
     return (
         <>
             <Text style={styles.title}>My Profile</Text>
+            <Image style={styles.image} source={{uri: img}}/>
+            <Pressable onPress={openImagePickerAsync} style={[styles.border, styles.uploadBtn]}>
+                <Text>Upload new photo</Text>
+            </Pressable>
             <LinkToPage pageName={'Skills'} path={{pathname: '/Skills'}}/>
             <Text style={styles.subtitle}>Projects:</Text>
             {
@@ -31,5 +49,18 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: 'bold',
         marginBottom: 15
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 10
+    },
+    border: {
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    uploadBtn: {
+        padding:10,
+        margin:10
     }
 })
