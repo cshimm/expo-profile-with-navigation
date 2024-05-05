ed999fab96b486736585318f905dcef7cda21819
cshimm <cameronjshimmin@gmail.com>
2024-05-05 11:59:28 -0700

    added image picker to projects

--- app/projects/Project.js
+++ app/projects/Project.js
@@ -1,13 +1,30 @@
-import {View, Text, Image, StyleSheet} from 'react-native'
+import {View, Text, Image, StyleSheet, Pressable} from 'react-native'
 import BackButton from "../../components/BackButton";
 import {useLocalSearchParams} from "expo-router";
+import {useState} from "react";
+import {requestMediaLibraryPermissionsAsync, launchImageLibraryAsync} from "expo-image-picker";
 
 export default function Project() {
     const {name, image, desc} = useLocalSearchParams()
+    const [img, setImg] = useState(image);
+    let openImagePickerAsync = async () => {
+        let permissionResult = await requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
+        }
+
+        let pickerResult = await launchImageLibraryAsync();
+        setImg(pickerResult.assets[0].uri)
+    }
     return (
         <View style={styles.container}>
             <Text>{name}</Text>
-            <Image style={styles.img} source={{uri:image && image}}/>
+            <Image style={styles.img} source={{uri:img && img}}/>
+            <Pressable onPress={openImagePickerAsync} style={[styles.uploadBtn, styles.border]}>
+                <Text>Upload photo</Text>
+            </Pressable>
             <Text>{desc}</Text>
             <BackButton/>
         </View>
@@ -16,12 +33,20 @@ export default function Project() {
 
 const styles = StyleSheet.create({
     img: {
-        width:50,
-        height: 50
+        width:150,
+        height: 150
     },
     container:{
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    border: {
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    uploadBtn: {
+        padding:10,
+        margin:10
     }
 })
